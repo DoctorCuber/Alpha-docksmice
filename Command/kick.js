e3fcf78ff96da863ed5307b53c79bd79904c2052
DoctorCuber <72154273+DoctorCuber@users.noreply.github.com>
2021-03-15 14:59:53 -0700

    ASDADASDASD

--- Command/kick.js
+++ Command/kick.js
@@ -1,15 +1,14 @@
 ﻿module.exports = {
-	name: 'Kick',
-	description: 'Kicks User',
-	execute(message, args) {
-		const member = message.mentions.users.first();
-		if (member) {
-			const memberTarget = message.guild.members.cache.get(member.id);
-			memberTarget.kick();
-			message.channel.send('User has been sucessfully kicked!');
-		} else {
-			message.channel.send('Error, unable to kick user, try mentioning a user!');
+    name: 'kick',
+    description: "This command kicks a member!",
+    execute(message, args) {
+        const target = message.mentions.users.first();
+        if (target) {
+            const memberTarget = message.guild.members.cache.get(target.id);
+            memberTarget.kick();
+            message.channel.send("User has been kicked");
+        } else {
+            message.channel.send(`You coudn't kick that member!`);
         }
-		
-	}
-};
+    }
+}
