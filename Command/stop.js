921754811d6b0ef5cfbb233ede8432b5a5029d93
DoctorCuber <72154273+DoctorCuber@users.noreply.github.com>
2021-03-18 07:57:58 -0700

    ..

--- Command/stop.js
+++ Command/stop.js
@@ -1,10 +1,13 @@
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
 module.exports = {
     name: "stop",
     description: "makes the bot stop playing music",
     async execute(message, args) {
-        const Discord = require('discord.js');
-        const client = new Discord.Client();
         const distube = require('distube');
+        client.distube = new distube(client, { searchSongs: false, emitNewSongOnly: true });
         if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
 
         let queue = await client.distube.getQueue(message);
