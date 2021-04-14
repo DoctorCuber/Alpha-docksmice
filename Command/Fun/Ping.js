11ab4dec5e4a25a282c857b34083f78ba6508b17
DoctorCuber <72154273+DoctorCuber@users.noreply.github.com>
2021-04-14 14:16:25 -0700

    Update Ping.js

--- Command/Fun/Ping.js
+++ Command/Fun/Ping.js
@@ -1,13 +1,13 @@
 module.exports={
     name:'ping',
     description: "Command ini digunakan untuk Ping",
-    execute(message, args, Discord, client){
+    execute(message,args, Discord, client){
           message.react("🏓");
 		let ping = new Discord.MessageEmbed()
 		.setColor("#00ff75")
-		.setTitle("Calculating Ping...");message.channel.send(ping).then((msg) => {
+		.setTitle("Calculating Ping...");message.channel.send(ping).then((poomsg) => {
                   let actualPing = new Discord.MessageEmbed().setColor("#00ff75").setTitle(`My ping is ${client.ws.ping}ms`);
-                  msg.edit(actualPing);
+                  poomsg.edit(actualPing);
             })
         }
   };
