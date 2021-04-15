ae6f6a069611f37a09dad8567415c1cb3627a3b2
DoctorCuber <72154273+DoctorCuber@users.noreply.github.com>
2021-04-14 17:14:38 -0700

    Depending on the ping, it will change the color of embed

--- Command/Fun/Ping.js
+++ Command/Fun/Ping.js
@@ -5,8 +5,16 @@ module.exports={
           message.react("🏓");
 		let ping = new Discord.MessageEmbed()
 		.setColor("#00ff75")
-		.setTitle("Calculating Ping...");message.channel.send(ping).then((poomsg) => {
-                  let actualPing = new Discord.MessageEmbed().setColor("#00ff75").setTitle(`My ping is ${client.ws.ping}ms`);
+		.setTitle("Calculating Ping...");
+            message.channel.send(ping).then((poomsg) => {
+                  let actualPing = new Discord.MessageEmbed().setTitle(`My ping is ${client.ws.ping}ms`);
+                  if(client.ws.ping >= 120){
+                        actualPing.setColor("#Ff0000");
+                  }else if(client.ws.ping <= 90){
+                        actualPing.setColor("#00FF24");
+                  }else {
+                        actualPing.setColor("#F2FF00");
+                  }
                   poomsg.edit(actualPing);
             })
         }
